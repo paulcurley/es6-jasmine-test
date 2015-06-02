5f41f5bb8a510b58fa4dc7ba07c67771dd138fba
Paul C <hello@paulcurley.co.uk>
2015-06-02 21:29:42 +0100

    fix export, call through, fail test

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 
-export class App {
+
+//import Base from '../src/base';
+
+//console.log(Base)
+
+export default class App {
     constructor (){
         this.name = 'test';
         console.log(this.name);
--- test/appSpec.js
+++ test/appSpec.js
@@ -3,11 +3,12 @@ import App from '../src/app';
 
 describe('App', () => {
     beforeEach(() => {
-        spyOn(App, "constructor")
+        spyOn(App, "constructor").and.callThrough();
+		var app = new App();
     });
 
     it('runs', () => {
-
+		
         expect(App.constructor).toHaveBeenCalled();
     });
 
